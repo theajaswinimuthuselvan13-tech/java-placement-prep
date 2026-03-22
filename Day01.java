2ca62eaa2adc61ac5a9729fc6dc3b4266bd3d35a
theajaswini <theajaswinimuthuselvan13@gmail.com>
2026-03-22 20:56:28 +0530

    Day 1: variables, if-else, loops, multiplication table

---
+++ Day01.java
@@ -0,0 +1,24 @@
+public class Day01{
+public static void main(String[] args){
+    int age =18;
+    double cgpa = 8.9;
+    String name="THEAJA";
+    boolean isStudent = true ;
+    System.out.println("Age:" +age);
+    System.out.println("CGPA:"+cgpa);
+    System.out.println("NAME:"+name);
+    System.out.println("IS STUDENT:"+isStudent);
+    if(age>=18){
+        System.out.println("adult");
+    }else{
+        System.out.println("minor");
+    }
+    for(int i=1 ; i<=5;i++){
+        System.out.println("6*"+i+"="+6*i);
+    }
+
+}
+}
+
+
+
