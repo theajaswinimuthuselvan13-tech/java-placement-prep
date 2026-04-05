ecd42ed7978ac4460dd8648ef7aef4ea0882a3bf
THEAJASWINI <theajaswinimuthuselvan13@gmail.com>
2026-04-06 00:40:49 +0530

    Day2 Kitchen class objects

---
+++ Kitchen.java
@@ -0,0 +1,10 @@
+public class Kitchen {
+    String recipe;
+    int cost;
+    double starrating;
+    void print(){
+        System.out.println("Recipe:"+recipe);
+        System.out.println("COST of recipe "+cost);
+        System.out.println("Rating so far : "+starrating);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main (String[] args ){
+       
+         Kitchen k1=new Kitchen();
+        k1.recipe="PANI PURI";
+        k1.cost=30;
+        k1.starrating=4.5;
+        k1.print();
+         Kitchen k2=new Kitchen();
+        k2.recipe="TEA";
+        k2.cost=10;
+        k2.starrating=4.5;
+        k2.print();
+    }
+}
